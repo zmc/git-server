8d2442bfb4b8bca72a46022005b554f478b27289
Zack Cerza <zack@redhat.com>
2022-11-23 11:59:08 -0700

    Add init container
    This container will take a list of repos in $REPOS, and clone them.
    
    Signed-off-by: Zack Cerza <zack@redhat.com>

---
+++ clone.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -ex
+REPOS=(${REPOS})
+REPO_AGE=${REPO_AGE:-"1 month"}
+cd /git
+set +e
+for repo in ${REPOS[@]}; do
+  git clone --mirror --shallow-since "$REPO_AGE" --shallow-submodules --no-single-branch $repo || \
+  git clone --mirror --depth 10 --shallow-submodules --no-single-branch $repo
+done
